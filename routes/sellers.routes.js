b34e59281fb4fe507087853e31831df2d8a5e194
umar-hazelsoft <97450937+umar-hazelsoft@users.noreply.github.com>
2022-01-23 12:17:42 +0500

    authVerificationMiddleware added to add new seller route

--- routes/sellers.routes.js
+++ routes/sellers.routes.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const sellersController = require('../controllers/sellers.controller');
+const authVerificationMiddleware = require('../middlewares/authVerification.middleware');
 
-router.post('', sellersController.addSeller);
+router.post('', authVerificationMiddleware, sellersController.addSeller);
 router.get('', sellersController.getSellers);
 router.get('/:id', sellersController.getSeller);
 
