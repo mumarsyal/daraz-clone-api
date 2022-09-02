6fcb53860fdc2d2d8b3a72c1bcc5ebb82d5565fe
umar-hazelsoft <umar.syal@atomicity-interactive.com>
2022-09-02 15:53:43 +0500

    test

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', 'localhost:8088');
 	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
